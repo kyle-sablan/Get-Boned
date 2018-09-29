76abbd55a8c6f989425c7d1730710d88a9dec395
kyle_sablan <kyle.sablan@gmail.com>
2018-09-29 15:32:11 -0700

    System for layering tilemaps
    Basic creation of levels using layered tilemaps

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    //Public Variables
+    public float moveSpeed;
+
+    //Private Variables
+    private Rigidbody2D myRB;
+
+    void Start()
+    {
+        myRB = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+	// Update is called once per frame
+	void Update ()
+    { 
+        //Movement on x-axis
+        //With input
+		if(Input.GetAxisRaw("Horizontal") < -0.5f || Input.GetAxisRaw("Horizontal") > 0.5f)
+        {
+            myRB.velocity = new Vector2(moveSpeed * Input.GetAxisRaw("Horizontal"), myRB.velocity.y);
+        }
+
+        //Without input
+        if (Input.GetAxisRaw("Horizontal") > -0.5f && Input.GetAxisRaw("Horizontal") < 0.5f)
+        {
+            myRB.velocity = new Vector2(0f, myRB.velocity.y);
+        }
+
+    }
+}
